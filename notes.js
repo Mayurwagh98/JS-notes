5d368619ce3b81227ba2812a0b64488db24b2e49
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-05-12 16:55:29 +0530

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,14 @@
+/*values that aren’t
+already of an expected type are often coerced to that type. The if
+statement expects a boolean, but if you pass it something that’s not
+already boolean, coercion will occur.*/
+
+// Falsy values in JS = 0 or ""
+
+// Truthy values in JS = 99 or "free"
+
+// Loops Definition - Repeating a set of actions until a certain condition fails—in other
+// words, repeating only while the condition holds
+
+// Iteration - Each time the loop block executes, that’s called an iteration.
+
